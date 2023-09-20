89d99f3a2cdc7bf2b1b48b66101a5f18a1bd04f9
Yiseo Kwon <yiseo1122@gmail.com>
2023-09-20 03:09:49 -0500

    feat: Created seeds, handlebars for home, login and post lists

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,12 @@
-const router = require('express').Router();
+const router = require("express").Router();
+// const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes.js");
 
-router.get("/", (req, res) => {
-    res.render("home");
+// router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
 });
 
 module.exports = router;
